853860cee5f0d9364be523183e15e3b4b83393ce
vadymdev <kravchenkobiz22@gmail.com>
2024-07-08 09:56:13 +0300

    fonts fixes

--- header.php
+++ header.php
@@ -4,9 +4,6 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<!--    <link rel="preconnect" href="https://fonts.googleapis.com">-->
-<!--    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>-->
-<!--    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">-->
     <title><?php echo custom_get_page_title(); ?></title>
     <?php wp_head(); ?>
 </head>
